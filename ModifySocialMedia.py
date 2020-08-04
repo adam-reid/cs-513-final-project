c1342669f82711f89787ba7a53abb488574f2fb0
Matthew Schley <matthewschley@Matthews-MacBook-Pro.local>
2020-08-03 19:10:52 -0500

    Social Media hyperlink cleaning - fb and twitter for now. no fb name condensing at the moment

---
+++ ModifySocialMedia.py
@@ -0,0 +1,27 @@
+import csv
+import os
+
+with open('PostMattChanges.csv') as infile, open('PostMattChangesNew.csv','w') as outfile:
+    writer = csv.writer(outfile)
+    for row in csv.reader(infile):
+        output_row = row
+        
+        if(row[4].startswith('twitter.com')):
+            #print(row[4])
+            output_row[4] = 'https://www.' + row[4]
+        if(row[4].startswith('@')):
+            #print(row[4][1:])
+            output_row[4] = 'https://www.twitter.com/' + row[4][1:]
+        if(row[4].startswith('www.twitter.com')):
+            #print(row[4])
+            output_row[4] = 'https://' + row[4]
+        if(row[3].startswith('facebook.com') | row[3].startswith('Facebook.com')):
+            output_row[3] = 'https://www.' + row[3]
+        if(row[3].startswith('@')):
+            #print(row[4][1:])
+            output_row[3] = 'https://www.facebook.com/' + row[3][1:]
+        if(row[3].startswith('www.')):
+            #print(row[4][1:])
+            output_row[3] = 'https://' + row[3]
+        
+        writer.writerow(output_row)
